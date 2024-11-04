eeea93eef57fbf20ffdb015173f5c3c940d2a04a
FernandaLoVacco <fefelovacco@gmail.com>
2024-11-04 15:29:36 -0300

    first commit

---
+++ crudtestdev/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using crudtestdev.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudtestdev.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options): base(Options) 
+        {
+        }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ crudtestdev/Models/Produtos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace crudtestdev.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+
+        [Display(Name ="Código")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string? Nome { get; set; }
+
+    }
+}
